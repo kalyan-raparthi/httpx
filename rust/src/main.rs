7cae75c56e637fa1a8fcbbcf3620e8bc873ac934
qb <kalyan.raparthi@hotmail.com>
2025-01-18 01:31:49 +0530

    get method added

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,9 +9,7 @@ fn get_file_size(path: &str) -> std::io::Result<u64> {
     Ok(metadata.len())
 }
 
-fn response(
-    stream: TcpStream) {
-    // CLIENT BUFFERED READER
+fn response(stream: TcpStream) {
     let mut reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
     
@@ -27,7 +25,6 @@ fn response(
         }
         headers_str.push_str(&line);
     }
-
     let _headers = parse_headers_to_hashmap(&headers_str);
 
     match request.split_whitespace().next().unwrap() {
@@ -46,20 +43,35 @@ fn handle_get(writer: &mut BufWriter<&TcpStream>, path: &str) {
     }
 }
 
-fn file_exists(path: &str) -> bool {
-    std::fs::metadata(path).is_ok()
-}
-
 fn send_file(writer: &mut BufWriter<&TcpStream>, path: &str) {
-    let header = format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
-        get_file_size(path).unwrap()
-    );
-    writer.write_all(header.as_bytes()).expect("send_file: ERROR WHILE WRITING HEADERS TO CLIENT");
-
     let file = File::open(path).expect("ERROR WHILE OPENING FILE");
+    
+    let paths = path.split('/').last().unwrap();
+    let response_type = paths.split('.').last().unwrap();
+
+    writer.write_all(get_header(response_type, path).as_bytes()).expect("send_file: ERROR WHILE WRITING HEADERS TO CLIENT");
     std::io::copy(&mut BufReader::new(file), writer).expect("send_file: ERROR WHILE WRITING CONTENT TO CLIENT");
     writer.flush().expect("send_file: ERROR WHILE FLUSHING BUFFER");
+}
+
+fn main() {
+    let server_socket: TcpListener = TcpListener::bind("127.0.0.1:2025").expect("Failed to bind");
+    println!("SERVER LISTENING ON: http://{}", &server_socket.local_addr().unwrap());
+    
+    for stream in server_socket.incoming() {
+        match stream {
+            Ok(stream) => {
+                std::thread::spawn(move || {
+                    let _cli_addr = stream.peer_addr().expect("Failed to get client address");
+                    println!("CLIENT CONNECTED: {}", _cli_addr);``
+                    response(stream);
+                });
+            },
+            Err(e) => {
+                eprintln!("ERROR ACCEPTING CONNECTIONS => {e}");
+            }
+        }
+    }
 }
 
 pub fn parse_headers_to_hashmap(headers: &String) -> HashMap<String, String> {
@@ -75,6 +87,13 @@ pub fn parse_headers_to_hashmap(headers: &String) -> HashMap<String, String> {
     header_map
 }
 
+fn file_exists(path: &str) -> bool {
+    match std::fs::metadata(path) {
+        Ok(metadata) => metadata.is_file(),
+        Err(_) => false,
+    }
+}
+
 fn send_response(writer: &mut BufWriter<&TcpStream>, status_code: u16, status_message: &str, body: Option<&str>) {
     let body = body.unwrap_or("");
     let response = format!(
@@ -88,24 +107,43 @@ fn send_response(writer: &mut BufWriter<&TcpStream>, status_code: u16, status_me
     writer.flush().expect("send_response: ERROR WHILE FLUSHING BUFFER");
 }
 
-fn main() {
+fn get_header(response_type: &str, path: &str) -> String {
+    let content_type = match response_type {
+        "html" => "text/html",
+        "htm" => "text/html",
+        "css" => "text/css",
+        "js" => "application/javascript",
+        "json" => "application/json",
+        "xml" => "application/xml",
+        "png" => "image/png",
+        "jpg" => "image/jpeg",
+        "jpeg" => "image/jpeg",
+        "gif" => "image/gif",
+        "bmp" => "image/bmp",
+        "ico" => "image/x-icon",
+        "svg" => "image/svg+xml",
+        "tiff" => "image/tiff",
+        "webp" => "image/webp",
+        "mp3" => "audio/mpeg",
+        "wav" => "audio/wav",
+        "ogg" => "audio/ogg",
+        "mp4" => "video/mp4",
+        "avi" => "video/x-msvideo",
+        "mov" => "video/quicktime",
+        "pdf" => "application/pdf",
+        "zip" => "application/zip",
+        "tar" => "application/x-tar",
+        "rar" => "application/vnd.rar",
+        "7z" => "application/x-7z-compressed",
+        "txt" => "text/plain",
+        "md" => "text/markdown",
+        _ => "application/octet-stream",
+    };
     
-    let server_socket: TcpListener = TcpListener::bind("127.0.0.1:2025").expect("Failed to bind");
-    println!("SERVER LISTENING ON: http://{}", &server_socket.local_addr().unwrap());
-
-    for stream in server_socket.incoming() {
-        match stream {
-            Ok(stream) => {
-                std::thread::spawn(move || {
-                    let cli_addr = stream.peer_addr().expect("Failed to get client address");
-                    println!("CONNECTED TO {}", cli_addr);
-                    response(stream);
-                    println!("[CONNECTION CLOSED]");
-                });
-            },
-            Err(e) => {
-                eprintln!("ERROR ACCEPTING CONNECTIONS => {e}");
-            }
-        }
-    }
+    let content_length = get_file_size(path).expect("get_header: ERROR WHILE GETTING FILE SIZE");
+    
+    format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
+        content_type, content_length
+    )
 }
