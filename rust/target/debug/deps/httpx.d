0175f1ecd6427e9c1070b6ae9664483a141ea582
qb <kalyan.raparthi@hotmail.com>
2025-01-19 21:36:05 +0530

    modulation

--- rust/target/debug/deps/httpx.d
+++ rust/target/debug/deps/httpx.d
@@ -1,5 +1,6 @@
-C:\Users\Jahnavi\qb\httpx\rust\target\debug\deps\httpx.exe: src/main.rs
+C:\Users\Jahnavi\qb\httpx\rust\target\debug\deps\httpx.exe: src/main.rs src\util.rs
 
-C:\Users\Jahnavi\qb\httpx\rust\target\debug\deps\httpx.d: src/main.rs
+C:\Users\Jahnavi\qb\httpx\rust\target\debug\deps\httpx.d: src/main.rs src\util.rs
 
 src/main.rs:
+src\util.rs:
--- rust/target/debug/httpx.d
+++ rust/target/debug/httpx.d
@@ -1,1 +1,1 @@
-C:\Users\Jahnavi\qb\httpx\rust\target\debug\httpx.exe: C:\Users\Jahnavi\qb\httpx\rust\src\main.rs
+C:\Users\Jahnavi\qb\httpx\rust\target\debug\httpx.exe: C:\Users\Jahnavi\qb\httpx\rust\src\main.rs C:\Users\Jahnavi\qb\httpx\rust\src\util.rs
